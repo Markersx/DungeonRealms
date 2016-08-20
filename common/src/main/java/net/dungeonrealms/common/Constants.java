4c74a878a21a020363cbbe57eb2374978945c131
matt11matthew <matt11matthew@gmail.com>
2016-08-19 21:02:59 -0400

    Create Constants.java

---
+++ common/src/main/java/net/dungeonrealms/common/Constants.java
@@ -0,0 +1,15 @@
+package net.dungeonrealms.common;
+
+import java.util.logging.Logger;
+ 
+public class Constants {
+
+    public static boolean debug = false;
+
+    public static Logger log = Logger.getLogger(Constants.class.getName());
+
+    public static final String[] PREVIOUS_DEVELOPERS = new String[]{"Proxying", "Atlas__", "APOLLO_IO", "Bradez1571", "EtherealTemplar", "Xwaffle", "Necrone_"};
+    public static final String[] DEVELOPERS = new String[]{"xFinityPro", "Vawke", "Kayaba", "matt11matthew"};
+
+    public static int PLAYER_SLOTS = 150;
+}
