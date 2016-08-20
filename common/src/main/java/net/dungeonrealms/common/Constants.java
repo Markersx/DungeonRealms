5b297c32f1b1db028fae548861c858ce37916be6
Giovanni N <io.vawke@gmail.com>
2016-08-20 03:07:32 +0200

    Necrone & Brad don't deserve credit for changing some strings.
    noobs.

--- common/src/main/java/net/dungeonrealms/common/Constants.java
+++ common/src/main/java/net/dungeonrealms/common/Constants.java
@@ -8,7 +8,7 @@ public class Constants {
 
     public static Logger log = Logger.getLogger(Constants.class.getName());
 
-    public static final String[] PREVIOUS_DEVELOPERS = new String[]{"Proxying", "Atlas__", "APOLLO_IO", "Bradez1571", "EtherealTemplar", "Xwaffle", "Necrone_"};
+    public static final String[] PREVIOUS_DEVELOPERS = new String[]{"Proxying", "Atlas__", "APOLLO_IO", "EtherealTemplar", "Xwaffle"};
     public static final String[] DEVELOPERS = new String[]{"xFinityPro", "Vawke", "Kayaba", "matt11matthew"};
 
     public static int PLAYER_SLOTS = 150;
